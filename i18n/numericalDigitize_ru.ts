9826bd9a8c3ab259be386aee5fee5e66d48653e5
Alexander Lisovenko <alexander.lisovenko@gmail.com>
2015-03-27 10:40:47 +0400

    init commit

---
+++ i18n/numericalDigitize_ru.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>NdAddFeatureGui</name>
+    <message>
+        <location filename="nd_addfeaturegui.py" line="61"/>
+        <source>crs not selected</source>
+        <translation>не выбрана</translation>
+    </message>
+</context>
+<context>
+    <name>Nd_AddFeature</name>
+    <message>
+        <location filename="nd_addfeature.ui" line="17"/>
+        <source>Add numerical feature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="23"/>
+        <source>Add a numerical feature:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="31"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="36"/>
+        <source>X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="41"/>
+        <source>Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="49"/>
+        <source>Coordinates are given</source>
+        <translation>Координаты заданы в</translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="58"/>
+        <source>in the CRS of theProject</source>
+        <translation>в системе координат проекта</translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="68"/>
+        <source>in the CRS of the Layer</source>
+        <translation>в системе координат слоя</translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="92"/>
+        <source>other</source>
+        <translation>дугая системы координат</translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="99"/>
+        <source>WGS 2134</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="nd_addfeature.ui" line="112"/>
+        <source>Select</source>
+        <translation>Выбрать</translation>
+    </message>
+</context>
+</TS>
